fe1dc11839ed5424d49d596b35627824e1e46176
cmurphey8 <53098445+cmurphey8@users.noreply.github.com>
2022-03-14 14:23:50 -0400

    Add files via upload

--- KnightsTour.java
+++ KnightsTour.java
@@ -17,15 +17,13 @@ public class KnightsTour {
     public static final int[] xDiff = {1, -1, 2, 2, 1, -1, -2, -2};
     public static final int[] yDiff = {2, 2, 1, -1, -2, -2, 1, -1};
     public static final int numMoves = xDiff.length;
-    public static double[] moves;
     
     public static void main(String[] args) throws IOException {
         if (args.length == 1) ChessBoard.SLEEP = Integer.parseInt(args[0]);
         else ChessBoard.SLEEP = 100;
 
-        // declare chess board and move scoring arrays
+        // declare chess board array
         chess = new int[N][N];
-        moves = new double[numMoves];
 
         // run sims until we find a valid knight's tour!
         int count = 0;
@@ -49,13 +47,13 @@ public class KnightsTour {
         ChessBoard.initBoard(knightRow, knightCol);
 
         // true at each index where we have a valid move
-        allMoves(knightRow, knightCol);
+        int direction = findNext(knightRow, knightCol);
 
         // continue the tour so long as a valid move is possible
-        while(canMove()) {
+        while(direction < numMoves) {
             
             // select direction where we will have (nonzero) minimum number of possible next moves
-            int direction = findNext();
+            // int direction = findNext();
             knightCol += xDiff[direction];
             knightRow += yDiff[direction];
 
@@ -64,7 +62,7 @@ public class KnightsTour {
             ChessBoard.updateBoard(knightRow, knightCol);
 
             // update moves array from new location
-            allMoves(knightRow, knightCol);
+            direction = findNext(knightRow, knightCol);
         }
 
         // report whether the tour was completed, or stopped short
@@ -72,52 +70,29 @@ public class KnightsTour {
     }
 
     // find all possible moves from chess board index (kR, kC)
-    public static void allMoves(int kR, int kC)
+    public static int findNext(int kR, int kC)
     {   
+        int bestMove = Integer.MAX_VALUE;
+        double maxDist = Double.MIN_VALUE;
         // find all possible moves from index (kR, kC) in chess array
         for (int i = 0; i < numMoves; i++) {
 
             // if move i falls in range of our array indexes...
             boolean xCheck = kC + xDiff[i] < N && kC + xDiff[i] >= 0;
             boolean yCheck = kR + yDiff[i] < N && kR + yDiff[i] >= 0;
+            if (xCheck && yCheck) {
 
-            // separate this condition to avoid NullPointerException!
-            // && if move i does not revisit an index...
-            if (xCheck && yCheck && chess[kR + yDiff[i]][kC + xDiff[i]] == 0) {
+                // separate these conditions to avoid NullPointerException!
+                double compareDist = ChessBoard.distance(kR + yDiff[i], kC + xDiff[i]);
 
-                // calcuate distance from center of the board
-                moves[i] = ChessBoard.distance(kR + yDiff[i], kC + xDiff[i]);
+                // if move i does not revisit a cell, and is furthest from center of the board
+                if (chess[kR + yDiff[i]][kC + xDiff[i]] == 0 && compareDist > maxDist) {
+                    bestMove = i;
+                    maxDist = compareDist;
+                }
             } 
-
-            // else set distance to minimum
-            else moves[i] = Double.MIN_VALUE;
         }
-    }
-
-    // find the knight's next move: lands furthest from the center of the board
-    public static int findNext()
-    {   
-        int maxIndex = Integer.MAX_VALUE;
-        double maxDist = Double.MIN_VALUE;
-        for (int i = 0; i < numMoves; i++) {
-            if (moves[i] > maxDist) {
-                maxIndex = i;
-                maxDist = moves[i];
-            }
-        }
-        return maxIndex;
-    }
-
-    // confirm the knight still has at least one valid move
-    public static boolean canMove()
-    {   
-        // if a move has distance > Double.MIN_VALUE, it must be valid
-        for (int i = 0; i < moves.length; i++) {
-            if (moves[i] > Double.MIN_VALUE) {
-                return true;
-            }
-        }
-        return false;
+        return bestMove;
     }
 
     // check for a complete knight's tour
